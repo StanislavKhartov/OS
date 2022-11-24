9d03f2a05295fbf74da5789ef3f73a1bda754262
Stanislav <fpm.hartovSV@bsu.by>
2022-11-24 19:09:59 +0300

    I have done it

---
+++ Windows/Killer/ProcessKiller/ProcessKiller/ProcessKiller.cpp
@@ -0,0 +1,69 @@
+﻿#define _CRT_SECURE_NO_WARNINGS //для strpy в ручном режиме
+
+#include <Windows.h>
+#include <windef.h>
+#include <handleapi.h>
+#include <TlHelp32.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <regex>
+
+void endProcByName(std::string str) {
+	std::vector<DWORD> pids;
+	std::wstring targetProcessName(str.begin(), str.end());
+	HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); //all processes
+	PROCESSENTRY32W entry; //current process
+	entry.dwSize = sizeof entry;
+
+	if (!Process32FirstW(snap, &entry)) { //start with the first in snapshot
+		return;
+	}
+
+	do {
+		if (std::wstring(entry.szExeFile) == targetProcessName) {
+			pids.emplace_back(entry.th32ProcessID); //name matches; add to list
+		}
+	} while (Process32NextW(snap, &entry)); //keep going until end of snapshot
+
+	for (int i(0); i < pids.size(); ++i) {
+		HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pids[i]);
+		TerminateProcess(processHandle, 1);
+		CloseHandle(processHandle);
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	int mode = 3;
+	std::string str;
+	//std::cin >> mode >> str;
+	wchar_t wcstr[1000];
+	GetEnvironmentVariable(L"PROC_TO_KILL", wcstr, sizeof(wcstr));
+	std::wstring ws(wcstr);
+	std::string str1(ws.begin(), ws.end());
+	std::regex regex{ R"([,]+)" }; // split on space and comma
+	std::sregex_token_iterator it{ str1.begin(), str1.end(), regex, -1 };
+	std::vector<std::string> names{ it, {} };
+	for (auto i : names) {
+		endProcByName(i);
+	}
+	//int mode = atoi(argv[1]);
+	if (mode == 1) {
+
+		int n = str.length();
+		char char_array[100 + 1];
+		strcpy(char_array, str.c_str());
+
+		DWORD procID = atoi(char_array);
+		HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+		TerminateProcess(processHandle, 1);
+		CloseHandle(processHandle);
+		return 0;
+	}
+	if (mode == 2) {
+		endProcByName(str);
+	}
+	return 0;
+}
---
+++ Windows/Killer/USER/USER/USER.cpp
@@ -0,0 +1,17 @@
+﻿#ifdef _UNICODE
+#define SetConsoleTitle SetConsoleTitleW
+#else
+#define SetConsoleTitle SetConsoleTitleA
+#endif
+
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    SetEnvironmentVariable(L"PROC_TO_KILL", L"Telegram.exe,notepad.exe,chrome.exe");
+    STARTUPINFO startupinfo = { sizeof startupinfo };
+    PROCESS_INFORMATION processInformation = { 0 };
+    std::wstring ws(L"/HELP /NAME Telegram.exe /PID 21868 /ENVV /EXIT");
+    CreateProcess(L"..\\..\\ProcessKiller\\x64\\Debug\\ProcessKiller.exe", NULL, NULL, NULL, FALSE, NULL, NULL, NULL, &startupinfo,&processInformation);
+}
